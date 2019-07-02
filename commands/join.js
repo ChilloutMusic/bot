5ee9e3ad2beef63030ffedb2a56780291bce77ad
braincomb <zapper@gmail.com>
2019-07-02 13:08:14 -0700

    Fixed join command permissions

--- commands/join.js
+++ commands/join.js
@@ -1,5 +1,5 @@
 exports.name = 'join'
-exports.permission = config.permissions.admin
+exports.permission = config.permissions.none
 exports.enabled = true
 exports.handler = function(data) {
 	if (raffle.running) {
