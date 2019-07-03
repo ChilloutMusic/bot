4aaf81a2bcc0bbbf5591c2157fb15cc25f56da84
braincomb <zapper@gmail.com>
2019-07-03 09:15:24 -0700

    Minor scramble fixes.

--- games/scramble.js
+++ games/scramble.js
@@ -9,11 +9,11 @@ module.exports = function() {
 			if (!scramble.running) {
 				scramble.answer = scramble.words[Math.floor(Math.random() * scramble.words.length)];
 				bot.sendChat(":books: Scramble has started, You have " + scramble.timelimit + " seconds.");
-				bot.sendChat("Unscramble the word: " + scramble.shuffle(scramble.answer));
+				bot.sendChat("Unscramble the word: " + scramble.shuffle(scramble.answer.toUpperCase()));
 				scramble.running = true;
 				scramble.timeout = setTimeout(function() {
 					scramble.running = false;
-					bot.sendChat("Scramble ended! The word was: "+scramble.answer);
+					bot.sendChat("Scramble ended!");
 				}, scramble.timelimit*1000);
 			} else {
 				bot.sendChat("Scramble already running!");
@@ -31,9 +31,9 @@ module.exports = function() {
 		},
 		checkAnswer: function(data) {
 			if (data.message.toLowerCase() == scramble.answer) {
-				trivia.running = false;
+				scramble.running = false;
 				clearTimeout(scramble.timeout);
-				bot.sendChat("@" + data.from.username + " correct! The word was "+scramble.answer);
+				bot.sendChat("@" + data.from.username + " correct! The word was " + scramble.answer.toUpperCase());
 				bot.sendChat("Scramble ended! Thanks for playing.");
 			}
 		}
