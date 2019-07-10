0128e4da29ea0e594aaa391612a999d24db235af
Mitchdev <admin@mitchdev.net>
2019-07-10 15:49:14 +1200

    Added !skiptro
    Added !skiptro

---
+++ commands/skiptro.js
@@ -0,0 +1,8 @@
+exports.name = 'skiptro'
+exports.permission = config.permissions.none
+exports.enabled = true
+exports.handler = function(data) {
+	if ((bot.getDJ() !== null && bot.getDJ().id == data.from.id) || data.from.role > config.permissions.bouncer) {
+		bot.moderateForceSkip();
+	}
+}
