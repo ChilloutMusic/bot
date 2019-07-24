e84885859f010f6c60355392d7a5f8f1ab785703
braincomb <zapper@gmail.com>
2019-07-23 17:11:37 -0700

    Minor !tip fixes

--- commands/tip.js
+++ commands/tip.js
@@ -26,12 +26,14 @@ exports.handler = function(data) {
           isReceiverStaff = true;
         }
 
-        if (tipper.balance.coins >= tipAmount && isReceiverStaff && Math.sign(tipAmount) == 1) {
-          item.balance.coins = item.balance.coins + tipAmount;
-          bot.sendChat(item.username + " now has " + item.balance.coins + " ChillCoins(s)! :moneybag:");
-          hasEnoughCoins = true;
-        } else {
-          bot.sendChat("You wish! You don't have enough coins to give away :crying_cat_face:");
+        if (isReceiverStaff && Math.sign(tipAmount) == 1) {
+          if (tipper.balance.coins >= tipAmount) {
+            item.balance.coins = item.balance.coins + tipAmount;
+            bot.sendChat(item.username + " now has " + item.balance.coins + " ChillCoins(s)! :moneybag:");
+            hasEnoughCoins = true;
+          } else {
+            bot.sendChat("You wish! You don't have enough coins to tip :crying_cat_face:");
+          }
         }
 
         if (!isReceiverStaff && Math.sign(tipAmount) == 1) {
@@ -61,20 +63,35 @@ exports.handler = function(data) {
 	} else {
     // Award tip to current DJ
     if (bot.getDJ() && tipper.id != bot.getDJ().id) {
-      for (var i = 0; i < users.length; i++) {
-        var item = users[i];
-        if (bot.getDJ() != null && item.id == bot.getDJ().id) {
-          item.balance.coins = item.balance.coins + 1;
-          bot.sendChat(item.username + " received 1 ChillCoin for their amazing play! :moneybag:");
-          bot.woot();
-    
-          fs.writeFileSync("./users.json", JSON.stringify(users));
-          loadUsers(bot);
-          break;
-        }
+      if (data.from.role == 0 && tipper.balance.coins >= 1) {
+        tipper.balance.coins = tipper.balance.coins - 1;
+        tipDJ();
+      } else if (data.from.role >= config.permissions.bouncer) {
+        tipDJ();
+      } else {
+        bot.sendChat("You wish! You don't have enough coins to tip :crying_cat_face:");
       }
     } else {
       bot.sendChat("You can't tip yourself!");
     }
-	}
+  }
+  
+  function tipDJ() {
+    for (var i = 0; i < users.length; i++) {
+      var item = users[i];
+      if (bot.getDJ() != null && item.id == bot.getDJ().id) {
+        item.balance.coins = item.balance.coins + 1;
+        bot.sendChat(item.username + " received 1 ChillCoin for their amazing play! :moneybag:");
+        bot.woot();
+
+        updateUsers();
+        break;
+      }
+    }
+  }
+
+  function updateUsers() {
+    fs.writeFileSync("./users.json", JSON.stringify(users));
+    loadUsers(bot);
+  }
 }
